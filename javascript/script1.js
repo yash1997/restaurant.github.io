95fd17982e2f1d32d5d2c3ef9fb4bd48cc68fb9c
yash1997 <yashpatrange007@gmail.com>
2021-01-22 20:26:32 -0500

    Falvours of India

---
+++ javascript/script1.js
@@ -0,0 +1,10 @@
+
+//alert('JS attached Successfully !')
+
+/*let clk = document.getElementById('btn');
+let overlay = document.getElementById('overlay');
+
+clk.addEventListener('click', () => {
+    overlay.style.display= 'grid';
+    overlay.classList.add('animate-overlay')
+})*/
